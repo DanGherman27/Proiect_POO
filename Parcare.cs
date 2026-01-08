d6effdbf54270fefd9730e7e5af7cdbe46647985
Davinsonn <david-emanuel.galati@student.upt.ro>
2026-01-08 23:33:17 +0200

    Add files via upload

---
+++ Parcare.cs
@@ -0,0 +1,84 @@
+﻿namespace Parcare_1;
+
+public class Parcare
+{
+    public int IdParcare { get; set; }
+    public string AdresaParcare { get; set; }
+    public List<ZonaParcare> ListZoneParcare { get; set; }
+    
+    public int NrZoneParcare { get; set; }
+    public ReguliParcare Reguli { get; set; }
+
+    public Parcare(int idParcare, string adresaParcare, int nrZoneParcare)
+    {
+        IdParcare = idParcare;
+        AdresaParcare = adresaParcare;
+        NrZoneParcare = nrZoneParcare;
+        ListZoneParcare = new List<ZonaParcare>();
+    }
+    
+    public static void CreareLocParcare(ZonaParcare zona, int idLoc, TipLocParcare tipLoc)
+    {
+        LocDeParcare loc;
+
+        if (tipLoc == TipLocParcare.Standard)
+        {
+            loc = new LocStandard(idLoc);
+        }
+        else
+        {
+            loc = new LocPremium(idLoc);
+        }
+
+        loc.Ocupat = false;
+        zona.ListLocDeParcare.Add(loc);
+
+        Console.WriteLine("Loc de parcare creat cu succes!");
+    }
+    
+    public static void ModificareTipLocParcare(ZonaParcare zona, int idLoc, TipLocParcare tipNou)
+    {
+        var loc = zona.ListLocDeParcare
+            .FirstOrDefault(l => l.IdLocDeParcare == idLoc);
+
+        if (loc == null)
+        {
+            Console.WriteLine("Locul nu exista!");
+            return;
+        }
+
+        if (loc.TipLoc == tipNou)
+        {
+            Console.WriteLine("Locul are deja acest tip!");
+            return;
+        }
+
+        LocDeParcare locNou = tipNou switch
+        {
+            TipLocParcare.Standard => new LocStandard(idLoc),
+            TipLocParcare.Premium => new LocPremium(idLoc)
+        };
+
+        locNou.Ocupat = loc.Ocupat;
+
+        zona.ListLocDeParcare.Remove(loc);
+        zona.ListLocDeParcare.Add(locNou);
+
+        Console.WriteLine("Tipul locului a fost modificat!");
+    }
+    
+    public static void StergereLocParcare(ZonaParcare zona, int idLoc)
+    {
+        var loc = zona.ListLocDeParcare
+            .FirstOrDefault(l => l.IdLocDeParcare == idLoc);
+
+        if (loc == null)
+        {
+            Console.WriteLine("Locul de parcare nu exista!");
+            return;
+        }
+
+        zona.ListLocDeParcare.Remove(loc);
+        Console.WriteLine("Locul de parcare a fost sters!");
+    }
+}
